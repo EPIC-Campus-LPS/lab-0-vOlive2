cb9c9a4974be4f8e6f961c7757d874707cafbb99
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-17 12:07:54 -0600

    Create lab0file2.java

---
+++ lab0file2.java
@@ -0,0 +1,8 @@
+import java.util.Scanner
+public class lab0file2 {
+        public static void main(String[] args) {
+                Scanner scan = new Scanner (System.in);
+                int y = scan.nextInt();
+                System.out.println("You will turn 100 in "+(y+100));
+        }
+}
