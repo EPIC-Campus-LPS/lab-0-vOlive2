6db0d0f83dec373466beb8544fcabbfc2d08bd48
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-17 12:06:16 -0600

    typos

--- lab0file1.java
+++ lab0file1.java
@@ -1,4 +1,4 @@
-inport java.util.Scanner
+import java.util.Scanner
 public class lab0file1 {
         public static void main(String[] args) {
                 Scanner scan = new Scanner (System.in);
