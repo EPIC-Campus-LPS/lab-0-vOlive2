d678e8f1bfa84a493b14a9ee08338963030f655a
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-17 12:08:48 -0600

    removed space

--- lab0file2.java
+++ lab0file2.java
@@ -1,7 +1,7 @@
 import java.util.Scanner
 public class lab0file2 {
         public static void main(String[] args) {
-                Scanner scan = new Scanner (System.in);
+                Scanner scan = new Scanner(System.in);
                 int y = scan.nextInt();
                 System.out.println("You will turn 100 in "+(y+100));
         }
