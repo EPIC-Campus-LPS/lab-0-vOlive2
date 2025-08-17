85817b05ed450b8e36dfc5448e3a829134b07f48
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-17 12:08:23 -0600

    updated words

--- lab0file1.java
+++ lab0file1.java
@@ -1,12 +1,12 @@
 import java.util.Scanner
 public class lab0file1 {
         public static void main(String[] args) {
-                Scanner scan = new Scanner (System.in);
-                System.out.println("Please enter a Verb: ");
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Verb: ");
                 String verb = scan.next();
-                System.out.println("Please enter a Adjective: ");
+                System.out.println("Adjective: ");
                 String adjective = scan.next();
-                System.out.println("Please enter a Noun: ");
+                System.out.println("Noun: ");
                 String noun = scan.next();
                 System.out.println("They wanted to "+verb+" a "+adjective+" "+noun+" for the science fair");
         }
