ee7101162764a185634370a24fc7a919d57c0fbb
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-17 12:11:27 -0600

    Update lab0file2.java

--- lab0file2.java
+++ lab0file2.java
@@ -1,4 +1,4 @@
-import java.util.Scanner
+import java.util.Scanner;
 public class lab0file2 {
         public static void main(String[] args) {
                 Scanner scan = new Scanner(System.in);
