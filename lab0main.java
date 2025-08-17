98464d1722ffe87fdb6a21e5cd0e58b10314c406
vOlive2.0 <OLiveA148@lpsk12.org>
2025-08-17 12:05:37 -0600

    added main

---
+++ lab0main.java
@@ -0,0 +1,13 @@
+inport java.util.Scanner
+public class lab0main {
+        public static void main(String[] args) {
+                Scanner scan = new Scanner (System.in);
+                System.out.println("Please enter a Verb: ");
+                String verb = scan.next();
+                System.out.println("Please enter a Adjective: ");
+                String adjective = scan.next();
+                System.out.println("Please enter a Noun: ");
+                String noun = scan.next();
+                System.out.println("They wanted to "+verb+" a "+adjective+" "+noun+" for the science fair");
+        }
+}
